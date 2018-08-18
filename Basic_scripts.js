9ff88598e63162adf8cb722843ba59281264b8ff
ranusharma09@gmail.com <ranusharma09@gmail.com>
2018-08-19 00:18:58 +0200

    creatingDB_insertingData

---
+++ Basic_scripts.js
@@ -0,0 +1,5 @@
+db.getCollection('details').find({})
+
+db.details.insert([{first_name: "Vimal", Last_name: "Rawat"},{first_name: "Steven", Last_name: "Smith"},{first_name: "Brian", Last_name: "Adam"}]);
+db.details.find()
+
